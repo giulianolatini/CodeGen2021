473ed9c457759cdbceaa71257270a748a2f44f52
Giuliano Latini <latini.giuliano@gmail.com>
2021-02-13 18:30:21 +0100

    Add powershell profiles configuration

---
+++ Docs/Ps1_Profiles/pwsh_profile.ps1
@@ -0,0 +1,73 @@
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+Import-Module posh-docker
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+$ThemeSettings.PromptSymbols.PromptIndicator = [char]::ConvertFromUtf32(0x25B6)
+
+function ProjectIDE {
+  param ( 
+    $RootProjectPath
+  )
+  
+  if (!$RootProjectPath) {
+    Write-Host ("needed the project root path ")
+  }
+  else {
+    $CmdLineArguments = ""
+    $CmdLineArguments = " --user-data-dir=" + $RootProjectPath + "\.vscode"
+    [String]$CmdLineArguments = $CmdLineArguments + " --extensions-dir=" + $RootProjectPath + "\.vscode\extensions"
+    [String]$CmdLineArguments = $CmdLineArguments + " " + $RootProjectPath
+    $ProcessInfo = New-Object System.Diagnostics.ProcessStartInfo 
+    $ProcessInfo.FileName = "${ENV:ProgramFiles}\Microsoft VS Code\Code.exe" 
+    $ProcessInfo.RedirectStandardError = $true 
+    $ProcessInfo.RedirectStandardOutput = $true 
+    $ProcessInfo.UseShellExecute = $False 
+    $ProcessInfo.Arguments = "$CmdLineArguments"
+    $ProcessInfo.CreateNoWindow = $False
+    $ProcessInfo.Verb = "runas"
+    $Process = New-Object System.Diagnostics.Process 
+    $Process.StartInfo = $ProcessInfo
+    $Process.Start() | Out-Null
+    #$Process.WaitForExit()
+    $Output = $Process.StandardOutput.ReadToEnd()
+    $Output
+  }
+}
+
+Set-Alias -Name "code-prj" -Value ProjectIDE
+
+if ($host.Name -eq 'ConsoleHost') {
+  Import-Module PSReadLine
+  Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+  Set-PSReadLineKeyHandler -Key Tab -Function Complete
+  Set-PSReadLineKeyHandler -Chord 'Oem7','Shift+Oem7' `
+                           -BriefDescription SmartInsertQuote `
+                           -LongDescription "Insert paired quotes if not already on a quote" `
+                           -ScriptBlock {
+      param($key, $arg)
+  
+      $line = $null
+      $cursor = $null
+      [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+  
+      if ($line[$cursor] -eq $key.KeyChar) {
+          # Just move the cursor
+          [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+      }
+      else {
+          # Insert matching quotes, move cursor to be in between the quotes
+          [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
+          [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+          [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
+      }
+  }
+}
+
+Add-WindowsPSModulePath
